a6a06b163c34d47b0e332ec4c2198a8979135f4b
Björn Gerdau <bjoerngerdau@gmx.de>
2022-11-23 00:22:06 +0100

    feat: extend kafka roundtrip test

--- kafkatest/cluster_test.go
+++ kafkatest/cluster_test.go
@@ -2,9 +2,11 @@ package kafkatest_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/kernle32dll/testcontainers-go-canned/kafkatest"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/twmb/franz-go/pkg/kadm"
@@ -72,27 +74,48 @@ func shutdownCluster(t *testing.T, kafkaCluster *kafkatest.KafkaCluster) func() 
 }
 
 func testRoundTrip(t *testing.T, kafkaCluster *kafkatest.KafkaCluster) {
-	if err := createTopic(t, kafkaCluster, t.Name()); err != nil {
+	t.Log("Initializing kafka client")
+	seed, err := kafkaCluster.GetKafkaHostAndPort(context.Background())
+	if err != nil {
 		t.Fatal(err)
+	}
+
+	topicName := t.Name()
+	kgoClient, err := kgo.NewClient(
+		kgo.SeedBrokers(seed),
+		kgo.DialTLSConfig(kafkaCluster.ClientTLSConfig()),
+		kgo.ConsumeTopics(topicName),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create topic
+	if err := createTopic(t, kgoClient, topicName); err != nil {
+		t.Fatal(err)
+	}
+
+	// Send the test message
+	sent, err := sendTestMessage(t, kgoClient, topicName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	received, err := receiveMessage(t, kgoClient, topicName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if received != sent {
+		t.Errorf("Received unexpected message content, wanted %q, got %q", sent, received)
 	}
 }
 
-func createTopic(t *testing.T, kafkaCluster *kafkatest.KafkaCluster, topicName string) error {
+func createTopic(t *testing.T, kgoClient *kgo.Client, topicName string) error {
 	t.Helper()
 
 	t.Log("Initializing kafka admin client")
-	seed, err := kafkaCluster.GetKafkaHostAndPort(context.Background())
-	if err != nil {
-		return err
-	}
-
-	kadmClient, err := kadm.NewOptClient(
-		kgo.SeedBrokers(seed),
-		kgo.DialTLSConfig(kafkaCluster.ClientTLSConfig()),
-	)
-	if err != nil {
-		return err
-	}
+	kadmClient := kadm.NewClient(kgoClient)
 
 	t.Log("Trying to create topic....")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
@@ -102,4 +125,44 @@ func createTopic(t *testing.T, kafkaCluster *kafkatest.KafkaCluster, topicName s
 	}
 
 	return nil
+}
+
+func sendTestMessage(t *testing.T, kgoClient *kgo.Client, topicName string) (string, error) {
+	t.Helper()
+
+	t.Log("Sending message")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	msg := uuid.NewString()
+	record := &kgo.Record{Topic: topicName, Value: []byte(msg)}
+	if err := kgoClient.ProduceSync(ctx, record).FirstErr(); err != nil {
+		return "", fmt.Errorf("failed to send message: %w", err)
+	}
+
+	return string(record.Value), nil
+}
+
+func receiveMessage(t *testing.T, kgoClient *kgo.Client, topicName string) (string, error) {
+	t.Helper()
+
+	t.Log("Receive message")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fetches := kgoClient.PollFetches(ctx)
+	if err := fetches.Err(); err != nil {
+		return "", fmt.Errorf("failed to receive messages: %w", err)
+	}
+
+	if county := len(fetches); county != 1 {
+		return "", fmt.Errorf("received unexpected message count %d", county)
+	}
+
+	msg := fetches.Records()[0]
+	if msg.Topic != topicName {
+		return "", fmt.Errorf("received unexpected topic message for %q", msg.Topic)
+	}
+
+	return string(msg.Value), nil
 }
